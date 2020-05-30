8930f48bacda7b2e096e61df6daed7b747f91824
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-05-29 21:22:34 -0500

    [Add] To save the files

---
+++ models/user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Create the user of the APP
+"""
+from flask_sqlalchemy import SQLAlchemy
+from app_flask.login_app import app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    """Class to make the querys to the database"""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    last_name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    pwd = db.Column(db.String(255), nullable=False)
+
+     def __init__(self, first_name, last_name, email, pwd):
+        """Set the columns in atributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.pwd = pwd
+
+    def save(self):
+        """Save the new object into the data base"""
+        db.session.add(self)
+        db.session.commit()

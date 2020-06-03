f303318d6ee20e07fdf2ec0b641d789d33573a82
cristian0497 <cristian04.97@hotmail.com>
2020-06-03 02:28:45 +0000

    [TEST] User class

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Test File for user.py """
-from app_flask import user
-from flask import Flask
+from app_flask.user import User
+from flask import Flask, session
 import unittest
 from flask_sqlalchemy import SQLAlchemy
-
+import uuid
+import random
+import string
 
 
 class test_User(unittest.TestCase):
@@ -12,35 +14,40 @@ class test_User(unittest.TestCase):
     def setUp(self):
         """ Set the General values to start the test """
         app = Flask(__name__)
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rappilending_test:RappiLending_Test@localhost:3306/rappilending_test_db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rappilending_dev:RappiLending_Dev@localhost:3306/rappilending_dev_db'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.secret_key = 'RappiLending_Test'
         db = SQLAlchemy(app)
+        mail = ''.join(random.choice(string.ascii_lowercase) for i in range(6)) + '@email.com'
         self.db = db
-        obj = user.User('FirstName_test', 'LastName_test', 'test@test.com', '12345678')
-        setattr(obj, 'id', 1)
+        obj = User('FirstName_test', 'LastName_test', mail, '12345678')
         self.obj = obj
-        #print(self.db.__dict__.keys())
-        print("------------------")
-        #print(db.Query)
         
     def tearDown(self):
-        """ restore all changes necesary """
-        print("Proces TearDown--> Registering logs")
+        """ restore all changes """
         pass
     
     def test_rightCount(self):
         """ Testing the right amount of attributes """
-        # print(self.__dict__)
-        self.assertTrue('id' in self.obj.__dict__)
-        self.assertTrue('first_name' in self.obj.__dict__)
-        self.assertTrue('last_name' in self.obj.__dict__)
-        self.assertTrue('email' in self.obj.__dict__)
-        self.assertTrue('pwd' in self.obj.__dict__)
+        self.assertTrue(hasattr(self.obj, 'id'))
+        self.assertTrue(hasattr(self.obj, 'first_name'))
+        self.assertTrue(hasattr(self.obj, 'last_name'))
+        self.assertTrue(hasattr(self.obj, 'email'))
+        self.assertTrue(hasattr(self.obj, 'pwd'))
         
-    def test_createUser(self):
-        """ Method to test if the user is created """
-        print("Test To create a user")
-        #print(self.db)
+    def test_SaveUser(self):
+        """ Method to test if the user is created and saved"""
+        self.obj.save()
+        records = self.db.session.query(User).all()
+        for usr in records:
+            print(usr.email)
+            if usr.email == self.obj.email:
+                saved = True
+                break
+        self.db.session.query(User).filter(User.email == self.obj.email).delete()
+        self.db.session.commit()
+        self.assertTrue(saved);
+        
         
 
+            
